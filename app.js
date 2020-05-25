9842e16b49f6fe9e2a1cb914a2a1a1aad870859a
Gabriel Agustin Uriarte <gabri02.uri@gmail.com>
2020-05-26 00:28:23 +0200

    Change in styles

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const loginCheck = user => {
+    if (user) {
+        loggedInLinks.forEach(link => link.style.display = 'block');
+        loggedOutLinks.forEach(link => link.style.display = 'none');
+    } else {
+        loggedInLinks.forEach(link => link.style.display = 'none');
+        loggedOutLinks.forEach(link => link.style.display = 'block');
+    }
+}
+
 // SingUP
 const signupForm = document.querySelector('#signup-form');
 
@@ -136,10 +149,12 @@ auth.onAuthStateChanged(user => {
             .get()
             .then((snapshot) => {
                 setupPosts(snapshot.docs);
+                loginCheck(user);
             })
     } else {
         console.log('auth: sign out')
         setupPosts([])
+        loginCheck(user);
     }
 })
 
